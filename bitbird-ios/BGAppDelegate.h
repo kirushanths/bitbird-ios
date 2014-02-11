f6cc90335a93682f7f28ffd86735185365b20689
KirushanthS <kirushanth.2@gmail.com>
2014-02-10 21:32:30 -0500

    Initial

---
+++ bitbird-ios/BGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BGAppDelegate.h
+//  bitbird-ios
+//
+//  Created by Kirushanth on 2014-02-10.
+//  Copyright (c) 2014 BitGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bitbird-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bitbird-ios
+//
+//  Created by Kirushanth on 2014-02-10.
+//  Copyright (c) 2014 BitGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGAppDelegate class]));
+	}
+}

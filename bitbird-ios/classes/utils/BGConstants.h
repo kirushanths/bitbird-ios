147de77b3e054f180f925bb40609341d851c75a8
KirushanthS <kirushanth.2@gmail.com>
2014-02-10 21:45:18 -0500

    added base files

---
+++ bitbird-ios/classes/utils/BGConstants.h
@@ -0,0 +1,24 @@
+//
+//  SEConstants.h
+//
+//  Created by Kirushanth on 2013-05-23.
+//  Copyright (c) 2013 THM. All rights reserved.
+//
+
+#include "TargetConditionals.h"
+#include "KSMacros.h"
+#include "BGUIConstants.h"
+#import "UILabel+FlexibleHeight.h"
+#import "UILabel+LatoFont.h"
+
+#ifndef snapestate_mobile_ios_SEConstants_h
+#define snapestate_mobile_ios_SEConstants_h
+
+//#define SE_DEBUG TRUE
+
+#define TESTFLIGHT_TOKEN	@"5aa9e965-ceaf-4e46-9578-1c92bc295045"
+
+// Define this constant for AFNetworking to allow invalid ssl certificate for debugging
+#define AFNETWORKING_ALLOW_INVALID_SSL_CERTIFICATES 1
+
+#endif
